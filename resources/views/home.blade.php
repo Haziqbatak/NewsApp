5fd7b0f9dd527996a512e6ec1aa5f69e14c4ad86
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-03-04 14:43:46 +0700

    new changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,20 @@
             <h1>
                 Welcome {{ Auth::user()->name }}
             </h1>
-            <div></div>
+            <hr>
+            <div class="card">
+                <!-- List group with active and disabled items -->
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item disabled" aria-disabled="true">A disabled item</li>
+                </ul><!-- End ist group with active and disabled items -->
+
+            </div>
 
         </div>
-        <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button class="btn btn-danger">logout</button>
-        </form>
+        
     </div>
 @endsection
