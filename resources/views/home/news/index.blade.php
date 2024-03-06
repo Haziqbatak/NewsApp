621e756faf3a6ef6969473ad890fc30465653e08
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-03-06 20:31:56 +0700

    new main

--- resources/views/home/news/index.blade.php
+++ resources/views/home/news/index.blade.php
@@ -8,5 +8,13 @@
         
         <a href="{{ route('news.create') }}" class="btn btn-primer">Create</a>
 
+        <div class="container">
+            <div class="card p-3">
+                <h5 class="card-title p-3">Data news</h5>
+
+                <table class="table-datatable"></table>
+            </div>
+        </div>
+
     </div>
 @endsection
