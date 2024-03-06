2cab6d9cfef9bdcdfed78c3338df90e9cc7695f1
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-03-06 14:17:25 +0700

    new

--- resources/views/home/includes/navbar.blade.php
+++ resources/views/home/includes/navbar.blade.php
@@ -168,10 +168,10 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" alt="Profile"
-                        class="rounded-circle">
+                    <img src="https://ui-avatars.com/api/?background=#fffff?name={{ Auth::user()->name }}"
+                        alt="Profile" class="rounded-circle">
                     <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
-                </a><!-- End Profile Iamge Icon -->
+                </a><!-- End Profile Image Icon -->
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                     <li class="dropdown-header">
