263cc4737879929cebc89dbee7c86254fbc858f7
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-03-13 10:53:40 +0700

    main

--- resources/views/home/profile/changePassword.blade.php
+++ resources/views/home/profile/changePassword.blade.php
@@ -1,7 +1,36 @@
 @extends('home.parent')
 
 @section('content')
+    <div class="row">
+        <div class="card p-4">
+            <h3 class="card-title">Change Password</h3>
+            <form action="" method="post">
+                @csrf
+                @method('PUT')
+                <div class="row mb-3">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Current Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" placeholder="Current Password">
+                    </div>
+                </div>
 
-ini halaman change password
+                <div class="row mb-3">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" placeholder="New Password">
+                    </div>
+                </div>
 
+                <div class="row mb-3">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Confirm New Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" placeholder="Confirm New Password">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Change Password</button>
+
+            </form>
+        </div>
+    </div>
 @endsection
