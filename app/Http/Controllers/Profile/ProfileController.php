fc8cea61a44d312ac4c0cfdf2c9b81b77c2283ac
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-03-13 11:08:54 +0700

    new

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -19,5 +20,26 @@ class ProfileController extends Controller
         $title = 'changePassword';
 
         return view('home.profile.changePassword', compact('title'));
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate($request,[
+            'current_password' => 'required',
+            'password' => 'required',
+            'confirm_password' => 'required'
+        ]);
+
+        $currentPasswordStatus = Hash::check(
+            $request->current_password, auth()->user()->password
+        );
+
+        if($currentPasswordStatus){
+            if($request->password == $request->confirm_password){
+                
+            }
+        }else{
+            return redirect()->back()->with('error', 'Current Password is Cannot');
+        }
     }
 }
--- resources/views/home/profile/changePassword.blade.php
+++ resources/views/home/profile/changePassword.blade.php
@@ -10,21 +10,21 @@
                 <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Current Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="Current Password">
+                        <input type="password" name="cuurent_password" class="form-control" placeholder="Current Password">
                     </div>
                 </div>
 
                 <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="New Password">
+                        <input type="password" name="password" class="form-control" placeholder="New Password">
                     </div>
                 </div>
 
                 <div class="row mb-3">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Confirm New Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" placeholder="Confirm New Password">
+                        <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password">
                     </div>
                 </div>
 
